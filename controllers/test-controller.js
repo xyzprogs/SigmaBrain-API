52bc2be5efb4a256d4e47c14af7f6b100b79e125
kai <kaiwendeveloer78@gmail.com>
2021-10-17 11:48:33 -0400

    firebase admin setup

--- controllers/test-controller.js
+++ controllers/test-controller.js
@@ -1,8 +1,31 @@
-const auth = require('firebase/auth')
-
+var admin = require('firebase-admin');
+const HEADER_CONSTANT = require('../constant/header')
 testUser = async (req, res) => {
-    console.log(req.headers);
-    res.sendStatus(200);
+    console.log(req.headers)
+    token = req.headers[HEADER_CONSTANT.TOKEN]
+    if(!token){
+        return res.status(401).json({
+            error: "NO TOKEN FOUND"
+        })
+    }
+    try {
+        admin
+        .auth()
+        .verifyIdToken(token)
+        .then((decodedToken) => {
+            console.log("decoded token: " + decodedToken)
+            res.sendStatus(200);
+        }).catch((error)=>{
+            console.log("error " + error)
+            res.sendStatus(400);
+        })
+    } catch (error) {
+        res.status(500).json({
+            "error": "server initialization fail"
+        })
+    }
+
+
 }
 
 module.exports = {
